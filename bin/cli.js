731e76b0a11163081eb982f79b703e2927ffb6ff
Jiazhen Wu <97967159+wjz1029wjz@users.noreply.github.com>
2022-12-09 00:39:45 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,4 @@
-/*import minimist from 'minimist';
-import { roll } from "../lib/roll.js";
 
-
-const args = minimist(process.argv.slice(2));
-const sides = args.sides || 6;
-const dice = args.dice || 2;
-const rolls = args.rolls || 1;
-
-const result = roll(sides, dice, rolls);
-console.log(JSON.stringify({ sides: sides, dice: dice, rolls: rolls, results: result}));
-*/
 import minimist from 'minimist'
 import roll from "../lib/roll.js"
 
