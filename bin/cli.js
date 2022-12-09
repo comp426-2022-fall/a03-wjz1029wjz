aaa4fd701c65c294f4a17a73326a5ccb023dc5c6
Jiazhen Wu <97967159+wjz1029wjz@users.noreply.github.com>
2022-12-09 00:06:46 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -3,11 +3,11 @@ import { roll } from "../lib/roll.js";
 
 
 const args = minimist(process.argv.slice(2));
-const sides = args.sides? args.sides:6 ;
-const dice = args.dice? args.dice:2;
-const rolls = args.rolls? args.rolls:1;
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
 
 const result = roll(sides, dice, rolls);
-console.log(JSON.stringify(result));
+console.log(result);
 
 //try to make 100 commits to get the full participation credit :)
